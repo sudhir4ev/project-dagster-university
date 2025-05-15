386907d9916894157f0f9e2ad0fade693b35c774
Sudhir <>
2025-05-15 22:08:59 +0800

    wip: setup taxi zones asset db import

--- dagster_university/dagster_essentials/dagster_essentials/assets/trips.py
+++ dagster_university/dagster_essentials/dagster_essentials/assets/trips.py
@@ -1,6 +1,10 @@
 import requests
 from dagster_essentials.assets import constants
 import dagster as dg
+import duckdb
+import os
+from dagster._utils.backoff import backoff
+
 
 @dg.asset
 def taxi_trips_file() -> None:
@@ -15,6 +19,7 @@ def taxi_trips_file() -> None:
     ) as output_file:
         output_file.write(raw_trips.content)
 
+
 @dg.asset
 def taxi_zones_file() -> None:
     """
@@ -26,3 +31,64 @@ def taxi_zones_file() -> None:
 
     with open(constants.TAXI_ZONES_FILE_PATH, "wb") as output_file:
         output_file.write(raw_taxi_zones.content)
+
+
+@dg.asset(
+    deps=["taxi_trips_file"]
+)
+def taxi_trips() -> None:
+    """
+      The raw taxi trips dataset, loaded into a DuckDB database
+    """
+    query = """
+        create or replace table trips as (
+          select
+            VendorID as vendor_id,
+            PULocationID as pickup_zone_id,
+            DOLocationID as dropoff_zone_id,
+            RatecodeID as rate_code_id,
+            payment_type as payment_type,
+            tpep_dropoff_datetime as dropoff_datetime,
+            tpep_pickup_datetime as pickup_datetime,
+            trip_distance as trip_distance,
+            passenger_count as passenger_count,
+            total_amount as total_amount
+          from 'data/raw/taxi_trips_2023-03.parquet'
+        );
+    """
+
+    conn = backoff(
+        fn=duckdb.connect,
+        retry_on=(RuntimeError, duckdb.IOException),
+        kwargs={
+            "database": os.getenv("DUCKDB_DATABASE"),
+        },
+        max_retries=10,
+    )
+    conn.execute(query)
+
+
+@dg.asset(
+    deps=["taxi_zones_file"]
+)
+def taxi_zones() -> None:
+    query = f"""
+        create or replace table zones as (
+          select
+            LocationID as zone_id,
+            zone,
+            borough,
+            the_geom as geometry,
+          from '{constants.TAXI_ZONES_FILE_PATH}'
+        );
+    """
+
+    conn = backoff(
+        fn=duckdb.connect,
+        retry_on=(RuntimeError, duckdb.IOException),
+        kwargs={
+            "database": os.getenv("DUCKDB_DATABASE"),
+        },
+        max_retries=10,
+    )
+    conn.execute(query)
