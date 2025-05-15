b66a4f7e95f732b08096f06f55366c42edfe949a
Sudhir <>
2025-05-15 21:52:59 +0800

    wip: setup taxi zones asset

--- dagster_university/dagster_essentials/dagster_essentials/assets/trips.py
+++ dagster_university/dagster_essentials/dagster_essentials/assets/trips.py
@@ -14,3 +14,15 @@ def taxi_trips_file() -> None:
             constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb"
     ) as output_file:
         output_file.write(raw_trips.content)
+
+@dg.asset
+def taxi_zones_file() -> None:
+    """
+      The raw CSV file for the taxi zones dataset. Sourced from the NYC Open Data portal.
+    """
+    raw_taxi_zones = requests.get(
+        "https://community-engineering-artifacts.s3.us-west-2.amazonaws.com/dagster-university/data/taxi_zones.csv"
+    )
+
+    with open(constants.TAXI_ZONES_FILE_PATH, "wb") as output_file:
+        output_file.write(raw_taxi_zones.content)
